df82a8976324ecbb83044d5991125468e057b69a
Vincent Toups <vincent.toups@gmail.com>
2019-08-20 19:05:34 -0400

    Added handy aliases.

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias m='docker run -v `pwd`:/host -it mbmbam-ya m'
+alias n='docker run -v `pwd`:/host -it mbmbam-ya node'
+alias b='docker run -v `pwd`:/host -it mbmbam-ya bash'
+alias h='docker run -v `pwd`:/host -it mbmbam-ya hy'
